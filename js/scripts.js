3d9ca9581e8897285822c9e96ec081f1c2dbfd7a
JacobSutherland <jacobsutherland224@gmail.com>
2021-01-07 10:14:51 -0600

    button filtering added

--- js/scripts.js
+++ js/scripts.js
@@ -26,40 +26,78 @@ async function constrsuctUserCard(data, cb){
         const userCard = userCardHTML;
         gallerySection.insertAdjacentHTML('beforeend', userCard);
         const card = document.querySelectorAll('.card');
-        card[i].addEventListener('click', async () => {
-            if(card[i].classList.contains(`${currentDataIndex.name.first}`)){
-            console.log(card[i])
-            const activeCard = currentDataIndex;
-            await cb(activeCard);
-            }
+        card[i].addEventListener('click', () => {
+            const currentIndex = i;
+            cb(data, currentIndex); 
         })
     }
 }
 
-async function constructUserModal(data){
+async function constructUserModal(employee, currentIndex){
+    let modalContainer;
+    let numberOfEmployees;
+    for(let i = 0; i < employee.results.length; i++){
+        const currentEmployeeIndex = employee.results[i];
             const modalHTML = 
             `<div class="modal-container">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                 <div class="modal-info-container">
-                    <img class="modal-img" src='${data.picture.large}' alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${data.name.first}, ${data.name.last}</h3>
-                    <p class="modal-text">${data.email}</p>
-                    <p class="modal-text cap">${data.location.city}</p>
+                    <img class="modal-img" src='${currentEmployeeIndex.picture.large}' alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${currentEmployeeIndex.name.first}, ${currentEmployeeIndex.name.last}</h3>
+                    <p class="modal-text">${currentEmployeeIndex.email}</p>
+                    <p class="modal-text cap">${currentEmployeeIndex.location.city}</p>
                     <hr>
-                    <p class="modal-text">${data.phone}</p>
+                    <p class="modal-text">${currentEmployeeIndex.phone}</p>
                     <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                    <p class="modal-text">Birthday: ${data.dob.date}</p>
+                    <p class="modal-text">Birthday: ${currentEmployeeIndex.dob.date}</p>
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+            </div>
             </div>
         </div>`
-                
             const userModal = modalHTML;
             body.insertAdjacentHTML('beforeend', userModal);
-            const exitModal = document.querySelector('.modal-close-btn')
-            const modalContainer = document.querySelector('.modal-container')
-            exitModal.addEventListener('click', () => {
-                body.removeChild(modalContainer);
-        })
+            const exitModal = document.querySelectorAll('.modal-close-btn')
+            modalContainer = document.querySelectorAll('.modal-container')
+            const modal = document.querySelectorAll('.modal')
+            const card = document.querySelectorAll('.card')
+            const modalPrevBtn = document.querySelectorAll('.modal-prev')
+            const modalNextBtn = document.querySelectorAll('.modal-next')
+            numberOfEmployees = card.length;
+
+            modalContainer[i].style.display = 'none';
+
+            exitModal[i].addEventListener('click', () => {
+                modalContainer.forEach( i => {
+                    body.removeChild(i);
+                })
+             })
+
+            modalPrevBtn[i].addEventListener('click', () => {
+                let previous = i;
+                previous--;
+                modalContainer[previous].style.display = 'block';
+                modalContainer[i].style.display = 'none';
+            });         
+
+            modalNextBtn[i].addEventListener('click', () => {
+                let next = i;
+                next++;
+                console.log(next)
+                modalContainer[next].style.display = 'block';
+                modalContainer[i].style.display = 'none';
+            })
+        }
+        for(let j = 0; j < numberOfEmployees; j++){
+            if(j !== currentIndex){
+                modalContainer[j].style.display = 'none';
+            } else {
+                modalContainer[j].style.display = 'block'
+            }
+    }
 }
 
 
