ff25fc2a53ee1c47487db8274d0d480032ff1d31
JacobSutherland <jacobsutherland224@gmail.com>
2021-01-25 03:24:17 -0600

    fixed search to find partial matches and layout break

--- js/search.js
+++ js/search.js
@@ -10,6 +10,11 @@ document.querySelector(".search-container").innerHTML = `
 const searchBtn = document.querySelector('.search-submit');
 const searchField = document.querySelector('.search-input');
 
+const messageContainer = document.createElement('div');
+        const message = document.createElement('p');
+        message.textContent = 'No Results';
+        messageContainer.appendChild(message);
+
 //accepts data and reformats it into an array containing only the first and last name results
 function listActiveEmployees(data){
     const activeEmployees = data.results.map( i => {
@@ -20,13 +25,13 @@ function listActiveEmployees(data){
 
 //accepts the array of employee names to compare against the text values inside the cards H3 tags
 function searchEmployees(employees){
-    const cardNames = document.querySelectorAll('.card-name')
-    const card = document.querySelectorAll('.card')
+    const cardNames = document.querySelectorAll('.card-name');
+    const card = document.querySelectorAll('.card');
+    let matches = 0;
 
-    searchBtn.addEventListener('click', () => {
-        let nonMatches = 0;
+    searchBtn.addEventListener('click', (e) => {
+        e.preventDefault();
         const userInput = searchField.value.toLowerCase();
-        const firstInputLetter = userInput[0]
         const filteredNames = employees.filter( i => {
             return i.includes(userInput);
         });
@@ -34,42 +39,29 @@ function searchEmployees(employees){
         if(searchField.value === ''){
             return false;
         }
-            //loops every card and checks if the card at the index matches, and if they start with the same letter to reduce partial matches, finally hiding and showing them if they meet the conditions
-            for(let i = 0; i < gallerySection.children.length; i++){
-                let firstCardLetter = cardNames[i].textContent.toLowerCase()[0];
-                if(filteredNames.includes(cardNames[i].textContent.toLowerCase()) && firstCardLetter === firstInputLetter){
-                    card[i].style.display = 'block'
-                    } else {
-                        card[i].style.display = 'none'
-                        nonMatches += 1;
-                    }
-                    //creates a message to tell user that no results were returned from a search
-                    const messageContainer = document.createElement('div');
-                    const message = document.createElement('p');
-                    message.textContent = 'No Results'
-                    messageContainer.appendChild(message);
-                    if(nonMatches > gallerySection.children.length - 1 && body.contains(messageContainer) === false){
-                    body.appendChild(messageContainer);
-                    } 
-                    //removes the message when a new search is made
-                    searchField.addEventListener('change', () => {
-                        if(body.contains(messageContainer)){
-                            body.removeChild(messageContainer);
-                        }
-                })
-                //removes the message if  a search made and the input hasn't changed (prevents multiple messages)
-                searchBtn.addEventListener('click', () => {
-                    if(body.contains(messageContainer)){
-                        body.removeChild(messageContainer);
-                    }
-            })
-            }
+    
+        //loops every card and checks if the card at the index matches, finally hiding and showing them if they meet the conditions
+        for(let i = 0; i < gallerySection.children.length; i++){
+            if(filteredNames.includes(cardNames[i].textContent.toLowerCase())){
+                card[i].style.display = 'flex';
+                matches += 1;
+                } else {
+                    card[i].style.display = 'none';
+                }
+        }
+        if(matches === 0 && body.contains(messageContainer) === false){
+            body.appendChild(messageContainer);
+        } else if(matches > 0 && body.contains(messageContainer)){
+            body.removeChild(messageContainer)
+        } 
+        matches = 0;
     })
     //empties search text field and displays all results when field is emptied
     searchField.addEventListener('change', () => {
         if(searchField.value === ''){
+            body.removeChild(messageContainer)
             for(let i = 0; i < gallerySection.children.length; i++){
-                card[i].style.display = 'block';
+                card[i].style.display = 'flex';
             }
         }
     })
